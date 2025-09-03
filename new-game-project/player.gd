a3c8ede5187458b0901db764d257108521406d53
tra <tralegendary99@gmail.com>
2025-09-03 16:41:40 +0700

    ...

---
+++ new-game-project/player.gd
@@ -0,0 +1,50 @@
+extends Area2D
+signal hit
+
+@export var speed = 400
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	#hide()
+
+func _on_body_entered(body):
+	hide() # player disappear after being hit
+	hit.emit()
+	#
+	$CollisionShape2d.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+		
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D2.play()
+	else: 
+		$AnimatedSprite2D2.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D2.animation = "walk"
+		#$AnimatedSprite2D2.flip_v = false
+		
+		$AnimatedSprite2D2.flip_h = velocity.y < 0
+	elif velocity.y != 0:
+		#$AnimatedSprite2D2.animation = "up"
+		$AnimatedSprite2D2.flip_v = velocity.y > 0
+		
